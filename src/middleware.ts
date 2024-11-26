812b331ad96950cad1b5ecd8e86548960a743599
Jezebel1990 <jezebelguedes@gmail.com>
2024-11-26 01:12:01 -0300

    feat: Add navbar components

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,7 @@
-export { default } from "next-auth/middleware";
-
-export const config = { matcher: ["/"] };
+import { withAuth } from "next-auth/middleware";
+export default withAuth({
+    secret: process.env.NEXTAUTH_URL,
+});
+export const config = { 
+    matcher: ["/"], 
+}
